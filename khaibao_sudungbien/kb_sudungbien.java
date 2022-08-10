8bd09a4b6952098e229b3a289e52a1d35c743350
LEBALOC2002 <108645216+LEBALOC2002@users.noreply.github.com>
2022-08-10 10:50:40 +0700

    khai báo và sử dụng biến

---
+++ khaibao_sudungbien/kb_sudungbien.java
@@ -0,0 +1,19 @@
+package khaibao_sudungbien;
+
+public class kb_sudungbien {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "HÀ NỘI";
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
+
