0fef5d6cafec2c1846c6780672442ff488cad211
LEBALOC2002 <108645216+LEBALOC2002@users.noreply.github.com>
2022-08-11 10:50:15 +0700

    tính lãi xuất khi vay

---
+++ Introduction to Java/src/numbers_into_letters/numbers_into_letters.java
@@ -0,0 +1,95 @@
+package numbers_into_letters;
+
+import java.util.Scanner;
+
+public class numbers_into_letters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (1 > 0) {
+            System.out.println("nhập số cần đọc (0-999): ");
+            String input = sc.nextLine();
+            while (!(input.matches("^[1-9]?[0-9]?[0-9]$"))) {
+                System.out.println("không hợp lệ!");
+                System.out.print("nhập số cần đọc (0-999): ");
+                input = sc.nextLine();
+            }
+            System.out.println(input);
+            String[] inputRay = input.split("");
+            switch (inputRay.length - 1) {
+                case 1:
+                    System.out.println(tenDown(inputRay[1]));
+                    break;
+                case 2:
+                    System.out.println(tenUp(inputRay[1], tenDown(inputRay[2])));
+                    break;
+                case 3:
+                    if ((inputRay[2].equals("0")) && (inputRay[3].equals("0"))) {
+                        System.out.println(hunredUp(inputRay[1]));
+                    } else {
+                        System.out.println(inputRay[1]);
+                        System.out.print(hunredUp(inputRay[1]));
+                        System.out.print(" and " + tenUp(inputRay[2], tenDown(inputRay[3]) + "\n"));
+                    }
+            }
+
+        }
+
+    }
+
+    private static String tenDown(String number) {
+        switch (number) {
+            case "0":
+                return "";
+            case "1":
+                return "one";
+            case "2":
+                return "two";
+            case "3":
+                return "three";
+            case "4":
+                return "four";
+            case "5":
+                return "five";
+            case "6":
+                return "six";
+            case "7":
+                return "seven";
+            case "8":
+                return "eight";
+            case "9":
+                return "nine";
+            default:
+                return "can't read this";
+        }
+    }
+
+    private static String tenUp(String number, String previous) {
+        switch (number) {
+            case "0":
+                return " ";
+            case "1":
+                switch (previous) {
+                    case "":
+                        return "ten";
+                    case "one":
+                        return "eleven";
+                    case "two":
+                        return "twelve";
+                    case "three":
+                        return "thirdteen";
+                    default:
+                        return previous + "teen";
+                }
+            case "2":
+                return "twenty " + previous;
+            case "3":
+                return "thirdty " + previous;
+            default:
+                return tenDown(number) + "ty" + " " + previous;
+
+        }
+    }
+    private static String hunredUp(String number) {
+        return tenDown(number) + " hunred";
+    }
+}
