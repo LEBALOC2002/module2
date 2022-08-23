740d1b5a0c6566a3323315b225e79f2cf28b18de
LEBALOC2002 <108645216+LEBALOC2002@users.noreply.github.com>
2022-08-23 14:27:20 +0700

    Triển khai interface 'Resizeable' cho các lớp hình học Bài tập

---
+++ Abstract Class and Interface/src/Implementation_interface_Resizeable_geometry_classes/Resizeable.java
@@ -0,0 +1,5 @@
+package Implementation_interface_Resizeable_geometry_classes;
+
+public interface Resizeable {
+    public abstract  void resize (double percent );
+}
---
+++ Abstract Class and Interface/src/Implementation_interface_Resizeable_geometry_classes/Square.java
@@ -0,0 +1,38 @@
+package Implementation_interface_Resizeable_geometry_classes;
+
+public class Square extends Shape{
+    private  double side ;
+    public Square (){
+
+    }
+    public Square (double side){
+        this.side = side ;
+    }
+    public Square (double side , String color , boolean filled ) {
+        super(color, filled);
+        this.side = side ;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+    public double getArea (){
+        return side* side ;
+    }
+    @Override
+    public String toString() {
+        return "A Square with side="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString()
+                + ", Area=" +
+                +getArea();
+    }
+    public void resize (double percent) {
+        this.side *= (percent/200);
+    }
+}
