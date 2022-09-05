7527f51ccf35717be5e855d851a8375fafdd397a
LEBALOC2002 <108645216+LEBALOC2002@users.noreply.github.com>
2022-09-05 14:37:21 +0700

    post 3 text File

---
+++ TextFile/src/bai3/Main.java
@@ -0,0 +1,7 @@
+package bai3;
+
+public class Main {
+    public static void main(String[] args) {
+        CopyFile copyFile = new CopyFile("src\\bai3\\file.txt ", "src\\bai3\\copyFile.txt");
+    }
+}
