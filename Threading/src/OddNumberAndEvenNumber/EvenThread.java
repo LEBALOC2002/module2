4130265793f7ba98dd37b997e27a03c7705d6f30
LEBALOC2002 <108645216+LEBALOC2002@users.noreply.github.com>
2022-09-06 15:02:19 +0700

    Post odd number and even number

---
+++ Threading/src/OddNumberAndEvenNumber/EvenThread.java
@@ -0,0 +1,21 @@
+package OddNumberAndEvenNumber;
+
+public class EvenThread implements Runnable {
+    private String name;
+
+    public EvenThread(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 2; i <= 10; i += 2) {
+                System.out.println(name + " number " + i);
+                Thread.sleep(15);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
